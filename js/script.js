0d07c1b3c955fc253b878231938422b818320a71
Tommaso Venza <tommasovenza@gmail.com>
2020-05-29 16:49:23 +0200

    prova codice all'interno di una funzione

--- js/script.js
+++ js/script.js
@@ -7,32 +7,51 @@
 // nel caso della parola     radar r = (0) a = (1) d = (2) a = (3) r = (4)      da sinistra verso destra
 //                           radar r = (0) a = (3) d = (2) a = (3) r = (4)      da destra verso sinistra 
 
-
 // posizione 1 = posizione 5
 
 // posizione 2 = posizione 4
 
 // posizione 3 = posizione 3
 
-
 //        (           fine appunti        )
+
 
 
 // inserita una parola ne ricavo l'array
 
 var parola = prompt('inserisci una parola');
-var parolaArray = [];
+
+
 
 // ciclo la parola per creare un array
 
-for (i = 0; i < parola.length; i++) {
+// for (i = 0; i < parola.length; i++) {
 
-    var letteraCorrente = (parola[i]);
+//     var letteraCorrente = (parola[i]);
 
-    console.log(letteraCorrente);
+//     console.log(letteraCorrente);
 
-    parolaArray.push(letteraCorrente);
-    console.log(parolaArray);
+//     parolaArray.push(letteraCorrente); 
+// }
+
+
+function leggiParola() {
+    for (i = 0; i < parola.length; i++) {
+
+        var parolaArray = [];
+        var letteraCorrente = (parola[i]);
+    
+        console.log(letteraCorrente);
+    
+        parolaArray.push(letteraCorrente); 
+
+        var risultato = parolaArray.push(letteraCorrente);
+
+        return risultato
+
+        
+    }
 }
 
-
+function leggiParola();
+console.log(risultato);
